8033dde47d06df03024ca3fcd9cad4e5f10d4192
msiva06 <m.sivasubramanian06@gmail.com>
2025-08-01 12:52:04 -0500

    Junit test cases of controller layer while mocking service layer

---
+++ src/test/java/com/project/community/controller/CommunityControllerTest.java
@@ -0,0 +1,68 @@
+package com.project.community.controller;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.community.model.CommunityType;
+import com.project.community.repository.CommunityRepository;
+import com.project.community.service.CommunityService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(CommunityController.class)
+class CommunityControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private CommunityService communityService;
+
+    @MockitoBean
+    private CommunityRepository communityRepository;
+
+    @InjectMocks
+    private CommunityController communityController;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void save() throws Exception {
+        CommunityDTO communityDTO = new CommunityDTO("Ashford", CommunityType.SINGLE_FAMILY.getDisplayName(),
+                true,(short)2,"Plainsboro","NJ","08536","100");
+        communityDTO.setCommunityId((short) 100);
+        when(communityService.save(any())).thenReturn(communityDTO);
+        mockMvc.perform(post("/community")
+                .content(objectMapper.writeValueAsString(communityDTO))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(content().string("100"));
+    }
+
+    @Test
+    void findByCommunityType() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteByCommunityId() {
+    }
+}

f76a71847b63fe5855565da02abbf22f21db2fd4
msiva06 <m.sivasubramanian06@gmail.com>
2025-12-27 20:57:21 -0600

    updated pom with common library dependency

--- src/main/java/com/project/community/CommunityApplication.java
+++ src/main/java/com/project/community/CommunityApplication.java
@@ -1,12 +1,29 @@
 package com.project.community;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CommunityApplication {
+public class CommunityApplication implements CommandLineRunner {
+
+	@Value("${spring.datasource.url}")
+	private  String url;
+
+	@Value("${spring.datasource.username}")
+	private String uname;
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Database URL: " + url);
+		System.out.println("Database Username: " + uname);
+	}
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CommunityApplication.class, args);
+
 	}
 }

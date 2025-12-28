2bb9624a537c1b2bf4edd35b274dc9e2efc96dc9
msiva06 <m.sivasubramanian06@gmail.com>
2025-12-27 21:00:21 -0600

    Updated Community and CommunityType entities

--- src/main/java/com/project/community/model/Community.java
+++ src/main/java/com/project/community/model/Community.java
@@ -1,16 +1,22 @@
 package com.project.community.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "community")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Community {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "community_id", unique = true, nullable = false)
-    private short communityId;
+    private int id;
 
     @Column(name = "community_name", nullable = false)
     private String communityName;
@@ -18,12 +24,6 @@ public class Community {
     @Enumerated(EnumType.STRING)
     @Column(name = "community_type", nullable = false)
     private CommunityType communityType;
-
-    @Column(name = "active_indicator", nullable = false)
-    private char activeIndicator;
-
-    @Column(name = "community_manager_id")
-    private short communityManagerId;
 
     @Column(name = "city",nullable = false)
     private String city;
@@ -38,10 +38,10 @@ public class Community {
     private String zip4;
 
     @Column(name = "created_timestamp", nullable = false)
-    private Timestamp createdTimestamp;
+    private LocalDateTime createdTimestamp;
 
     @Column(name = "updated_timestamp")
-    private Timestamp updatedTimestamp;
+    private LocalDateTime updatedTimestamp;
 
     @Column(name = "created_by", nullable = false)
     private String createdBy;
@@ -49,173 +49,4 @@ public class Community {
     @Column(name = "updated_by")
     private String updatedBy;
 
-    public Community() {
-
-    }
-
-    public Community(String communityName, short communityManagerId, String city, String state, String zip, String zip4) {
-        this.communityName = communityName;
-        this.communityManagerId = communityManagerId;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.zip4 = zip4;
-    }
-
-    public Community(String communityName, CommunityType communityType, char activeIndicator, short communityManagerId,
-                     String city, String state, String zip, String zip4, Timestamp createdTimestamp,
-                     Timestamp updatedTimestamp, String createdBy, String updatedBy) {
-        this.communityName = communityName;
-        this.communityType = communityType;
-        this.activeIndicator = activeIndicator;
-        this.communityManagerId = communityManagerId;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.zip4 = zip4;
-        this.createdTimestamp = createdTimestamp;
-        this.updatedTimestamp = updatedTimestamp;
-        this.createdBy = createdBy;
-        this.updatedBy = updatedBy;
-    }
-
-    public Community(short communityId, String communityName, char activeIndicator, short communityManagerId, String city, String state, String zip, String zip4, Timestamp createdTimestamp, Timestamp updatedTimestamp, String createdBy, String updatedBy) {
-        this.communityId = communityId;
-        this.communityName = communityName;
-        this.activeIndicator = activeIndicator;
-        this.communityManagerId = communityManagerId;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.zip4 = zip4;
-        this.createdTimestamp = createdTimestamp;
-        this.updatedTimestamp = updatedTimestamp;
-        this.createdBy = createdBy;
-        this.updatedBy = updatedBy;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public short getCommunityId() {
-        return communityId;
-    }
-
-    public String getCommunityName() {
-        return communityName;
-    }
-
-    public CommunityType getCommunityType() {
-        return communityType;
-    }
-
-
-
-    public short getCommunityManagerId() {
-        return communityManagerId;
-    }
-
-    public void setCommunityManagerId(short communityManagerId) {
-        this.communityManagerId = communityManagerId;
-    }
-
-    @Override
-    public String toString() {
-        return "Community{" +
-                "communityId=" + communityId +
-                ", communityName='" + communityName + '\'' +
-                ", communityType=" + communityType +
-                ", activeIndicator=" + activeIndicator +
-                ", communityManagerId=" + communityManagerId +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zip=" + zip +
-                ", zip4=" + zip4 +
-                ", createdTimestamp=" + createdTimestamp +
-                ", updatedTimestamp=" + updatedTimestamp +
-                ", createdBy='" + createdBy + '\'' +
-                ", updatedBy='" + updatedBy + '\'' +
-                '}';
-    }
-
-    public Timestamp getCreatedTimestamp() {
-        return createdTimestamp;
-    }
-
-    public Timestamp getUpdatedTimestamp() {
-        return updatedTimestamp;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public String getUpdatedBy() {
-        return updatedBy;
-    }
-
-    public void setCommunityId(short communityId) {
-        this.communityId = communityId;
-    }
-
-    public void setCommunityName(String communityName) {
-        this.communityName = communityName;
-    }
-
-    public void setCommunityType(CommunityType communityType) {
-        this.communityType = communityType;
-    }
-
-    public char getActiveIndicator() {
-        return activeIndicator;
-    }
-
-    public void setActiveIndicator(char activeIndicator) {
-        this.activeIndicator = activeIndicator;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-
-    public String getZip4() {
-        return zip4;
-    }
-
-    public void setZip4(String zip4) {
-        this.zip4 = zip4;
-    }
-
-    public void setCreatedTimestamp(Timestamp createdTimestamp) {
-        this.createdTimestamp = createdTimestamp;
-    }
-
-    public void setUpdatedTimestamp(Timestamp updatedTimestamp) {
-        this.updatedTimestamp = updatedTimestamp;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
 }
--- src/main/java/com/project/community/model/CommunityType.java
+++ src/main/java/com/project/community/model/CommunityType.java
@@ -2,22 +2,8 @@ package com.project.community.model;
 
 public enum CommunityType {
 
-        APARTMENTS("APARTMENTS"),
-        CONDOS("CONDOS"),
-        TOWNHOUSES("TOWNHOUSES"),
-        SINGLE_FAMILY("SINGLE FAMILY");
-
-        private String displayName;
-        CommunityType(String displayName) {
-                this.displayName = displayName;
-        }
-
-        public String getDisplayName() {
-                return displayName;
-        }
-
-        @Override
-        public String toString() {
-                return displayName;
-        }
+        APARTMENTS,
+        CONDOS,
+        TOWNHOUSES,
+        SINGLE_FAMILY
 }

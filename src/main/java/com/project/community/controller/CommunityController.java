79b7e36675d50ef9b15de95193992736e6df71fe
msiva06 <m.sivasubramanian06@gmail.com>
2025-12-27 20:59:13 -0600

    added createCommunity and Assign manager to community API endpoints

--- src/main/java/com/project/community/controller/CommunityController.java
+++ src/main/java/com/project/community/controller/CommunityController.java
@@ -1,38 +1,85 @@
 package com.project.community.controller;
 
+import com.project.community.DTO.CommunityDTO;
+import com.project.community.common.library.dto.UserDTO;
+import com.project.community.common.library.entity.Users;
+import com.project.community.common.library.service.CommUserDetails;
+import com.project.community.model.CommManager;
+import com.project.community.model.Community;
 import com.project.community.model.CommunityType;
+import com.project.community.repository.CommMgrRepository;
+import com.project.community.service.APIService;
 import com.project.community.service.CommunityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/community")
 public class CommunityController {
 
     @Autowired
     private CommunityService communityService;
 
-    @PostMapping(value = "/community", consumes = "application/json")
-    public ResponseEntity<Short> save(@RequestBody CommunityDTO communityDTO) {
-        CommunityDTO createdResponse = communityService.save(communityDTO);
-        return new ResponseEntity<>(createdResponse.getCommunityId(), HttpStatus.CREATED);
+    @Autowired
+    private APIService apiService;
+
+    @Autowired
+    private CommMgrRepository commMgrRepository;
+
+    @PostMapping(value = "/create", consumes = "application/json")
+    public ResponseEntity<?> save(@RequestBody CommunityDTO communityDTO) {
+        Community createdResponse = communityService.save(communityDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdResponse.getId());
     }
 
+    @PostMapping(value = "/{comm_id}/manager", consumes = "application/json")
+    public ResponseEntity<?> save(@PathVariable("comm_id") int comm_id ,@RequestBody UserDTO userDTO) {
+        var auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth == null){
+            return ResponseEntity.badRequest().body("Invalid JWT Token");
+        }
+
+        CommUserDetails userDetails = (CommUserDetails) auth.getPrincipal();
+        Users user = userDetails.getUser();
+        String role = user.getRoles().getRole();
+        if(!role.toUpperCase().equals("ADMIN")){
+            return ResponseEntity.badRequest().body("Not authorized to create user");
+        }
+        Users resp = apiService.callPostApi(userDTO);
+        if(resp == null){
+            return ResponseEntity.badRequest().body("Manager to community association failed");
+        }
+        int manager_id = resp.getId();
+        CommManager commManager = new CommManager();
+        commManager.setCommunityId(comm_id);
+        commManager.setManagerId(manager_id);
+        commMgrRepository.save(commManager);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(resp.getId());
+
+    }
+
+//    @PostMapping(value = "/{comm_id}/invite", consumes = "application/json")
+//    public ResponseEntity<?> sendInvite(@PathVariable("comm_id") int id, @RequestBody List<String> emails){
+//
+//    }
 
     @GetMapping("/community/{communityId}")
-    public ResponseEntity<CommunityDTO> findByCommunityId(@PathVariable("communityId") String communityId){
-        short id = Short.parseShort(communityId);
-        CommunityDTO communityDTO = communityService.findByCommunityId(id);
-        return new ResponseEntity<>(communityDTO,HttpStatus.OK);
+    public ResponseEntity<?> findByCommunityId(@PathVariable("communityId") String communityId){
+        int id = Integer.parseInt(communityId);
+        Community response = communityService.findByCommunityId(id);
+        return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
 
     @GetMapping("/community")
-    public ResponseEntity<List<CommunityDTO>> findByCommunityType(@RequestParam("communityType") String communityType){
-        List<CommunityDTO> commList = switch (communityType.toUpperCase()) {
+    public ResponseEntity<?> findByCommunityType(@RequestParam("communityType") String communityType){
+        List<Community> commList = switch (communityType.toUpperCase()) {
             case "APARTMENTS" -> communityService.findByCommunityType(CommunityType.APARTMENTS);
             case "TOWNHOUSES" -> communityService.findByCommunityType(CommunityType.TOWNHOUSES);
             case "CONDOS" -> communityService.findByCommunityType(CommunityType.CONDOS);
@@ -49,16 +96,14 @@ public class CommunityController {
 
     }
 
-
-
     @PutMapping(value = "/community" ,consumes = "application/json")
-    public ResponseEntity<CommunityDTO> update(@RequestBody CommunityDTO communityDTO){
-        CommunityDTO updated = communityService.update(communityDTO);
+    public ResponseEntity<?> update(@RequestBody int id, CommunityDTO communityDTO){
+        Community updated = communityService.update(id, communityDTO);
         return new ResponseEntity<>(updated, HttpStatus.OK);
     }
 
     @DeleteMapping("/community/{communityId}")
-    public ResponseEntity<Short> deleteByCommunityId(@PathVariable short communityId){
+    public ResponseEntity<?> deleteByCommunityId(@PathVariable int communityId){
         communityService.deleteByCommunityId(communityId);
         return new ResponseEntity<>(communityId,HttpStatus.OK);
     }

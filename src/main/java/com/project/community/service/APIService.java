2983d761502d3ef9ff4adf1479d49fcbbb238f5a
msiva06 <m.sivasubramanian06@gmail.com>
2025-12-27 21:05:41 -0600

    Added Service for Webclient

---
+++ src/main/java/com/project/community/service/APIService.java
@@ -0,0 +1,25 @@
+package com.project.community.service;
+
+import com.project.community.common.library.dto.UserDTO;
+import com.project.community.common.library.entity.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+public class APIService {
+
+    @Autowired
+    private WebClient webClient;
+
+    public Users callPostApi(UserDTO request) {
+        Users response =  webClient.post()
+                .uri("/auth/register")
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(Users.class)
+                .block();
+        return response;
+    }
+
+}

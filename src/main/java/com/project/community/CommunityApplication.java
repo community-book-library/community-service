bb7026b4d3d87c3317279f050be81647aba5d7e9
msiva06 <m.sivasubramanian06@gmail.com>
2026-01-09 20:01:36 -0600

    Removed the CommandLineRunner method

--- src/main/java/com/project/community/CommunityApplication.java
+++ src/main/java/com/project/community/CommunityApplication.java
@@ -1,25 +1,17 @@
 package com.project.community;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CommunityApplication implements CommandLineRunner {
+public class CommunityApplication {
 
 	@Value("${spring.datasource.url}")
 	private  String url;
 
 	@Value("${spring.datasource.username}")
 	private String uname;
-
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Database URL: " + url);
-		System.out.println("Database Username: " + uname);
-	}
 
 	public static void main(String[] args) {
 

7e485dacf8bb2cd814f0c6a87650c1e1be4fb5a3
msiva06 <m.sivasubramanian06@gmail.com>
2026-01-09 20:04:45 -0600

    Added service methods corresponding to Invite API endpoint

--- src/main/java/com/project/community/service/CommunityService.java
+++ src/main/java/com/project/community/service/CommunityService.java
@@ -1,15 +1,22 @@
 package com.project.community.service;
 
-import com.project.community.DTO.CommunityDTO;
+import com.project.community.common.library.entity.Roles;
+import com.project.community.dto.CommunityDTO;
+import com.project.community.dto.ManagerDTO;
 import com.project.community.model.Community;
 import com.project.community.model.CommunityType;
+import com.project.community.model.RegisterToken;
+import com.project.community.repository.CommRoleRepository;
 import com.project.community.repository.CommunityRepository;
+import com.project.community.repository.RegisterTokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CommunityService {
@@ -17,8 +24,17 @@ public class CommunityService {
     @Autowired
     private CommunityRepository communityRepository;
 
+    @Autowired
+    private CommRoleRepository commRoleRepository;
+
     @Value("${spring.application.name}")
     private String applicationName;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private RegisterTokenRepository registerTokenRepository;
 
     public Community save(CommunityDTO communityDTO){
         Community community = new Community();
@@ -76,5 +92,25 @@ public class CommunityService {
 
     public void deleteByCommunityId(int communityId) {
         communityRepository.deleteById(communityId);
+    }
+
+    public Optional<Community> checkCommunity(CommunityDTO communityDTO) {
+        Optional<Community> response =  communityRepository.findByCommunityNameAndCityAndZip(communityDTO.getCommunityName(),communityDTO.getCity(),communityDTO.getZip());
+        return response;
+    }
+
+    public String generateManagerRegisterURL(ManagerDTO managerDTO, String token) {
+        String hashed = passwordEncoder.encode(token);
+        List<Roles> roles = commRoleRepository.findAll();
+        Roles role = roles.stream().filter(rl -> rl.getRole().toUpperCase().equals(managerDTO.getAssignRole().toUpperCase())).findFirst().get();
+        RegisterToken reg = new RegisterToken();
+        reg.setCommunityId(managerDTO.getCommunityId());
+        reg.setUsername(managerDTO.getEmail());
+        reg.setToken(token);
+        reg.setStatus("ACTIVE");
+        reg.setRoleId(role.getRoleId());
+        registerTokenRepository.save(reg);
+        String registerLink = "http://localhost:8080/register/" + token;
+        return registerLink;
     }
 }

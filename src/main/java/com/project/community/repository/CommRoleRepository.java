d2696defb6a660edee3f8657866e24eedbcc483d
msiva06 <m.sivasubramanian06@gmail.com>
2026-01-11 21:34:06 -0600

    Added FindByRole method

--- src/main/java/com/project/community/repository/CommRoleRepository.java
+++ src/main/java/com/project/community/repository/CommRoleRepository.java
@@ -4,7 +4,10 @@ import com.project.community.common.library.entity.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CommRoleRepository extends JpaRepository<Roles,Integer> {
+    Optional<Roles> findByRole(String role);
 }
 

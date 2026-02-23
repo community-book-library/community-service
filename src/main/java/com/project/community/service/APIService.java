7d0c88bc15d40d6c12ff827d0cfadf63fb97bada
msiva06 <m.sivasubramanian06@gmail.com>
2026-02-23 10:32:17 -0600

    Included API service call for User endpoint

--- src/main/java/com/project/community/service/APIService.java
+++ src/main/java/com/project/community/service/APIService.java
@@ -3,14 +3,20 @@ package com.project.community.service;
 import com.project.community.common.library.dto.UserDTO;
 import com.project.community.common.library.entity.Users;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Optional;
 
 @Service
 public class APIService {
 
     @Autowired
     private WebClient webClient;
+
+    @Value("${usernamepath}")
+    private String usernamePath;
 
     public Users callPostApi(UserDTO request) {
         Users response =  webClient.post()
@@ -19,6 +25,15 @@ public class APIService {
                 .retrieve()
                 .bodyToMono(Users.class)
                 .block();
+        return response;
+    }
+
+    public Optional<Users> callFindByUsername(String username){
+        Optional<Users> response = Optional.ofNullable(webClient.get()
+                .uri(uriBuilder -> uriBuilder.path(usernamePath).queryParam("name", username).build())
+                .retrieve()
+                .bodyToMono(Users.class)
+                .block());
         return response;
     }
 

4d6d6b641fee8d8afb1c507ced31e343b3b4ff62
msiva06 <m.sivasubramanian06@gmail.com>
2026-02-23 10:26:32 -0600

    Updated JPARepository Annotation for external repository access

--- src/main/java/com/project/community/CommunityApplication.java
+++ src/main/java/com/project/community/CommunityApplication.java
@@ -3,8 +3,12 @@ package com.project.community;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {
+        "com.project.community"
+})
 public class CommunityApplication {
 
 	@Value("${spring.datasource.url}")
